c091e8af984b8f6ace44df309885f5a7b1f71d65
JeyaPrabha <jprabha877@gmail.com>
2025-02-21 13:08:35 +0530

    updated last7days tab in reports UI

--- src/Pages/Reports.jsx
+++ src/Pages/Reports.jsx
@@ -12,10 +12,28 @@ import LocalOfferOutlinedIcon from '@mui/icons-material/LocalOfferOutlined';
 import WorkspacesOutlinedIcon from '@mui/icons-material/WorkspacesOutlined';
 import TranslateOutlinedIcon from '@mui/icons-material/TranslateOutlined';
 import TicketReports from '../Component/Reports/TicketReports';
+import AutocompleteComponent from "../Component/AutocompleteComponent";
 
+const styles = {
+    newticketsAutocomplete: {
+        height: '32px',
+        border: '1px solid #c9c9cd',
+        width: '135px',
+        '&:hover': {
+            border: '1px solid blue !important',
+        },
+        '&.Mui-focused': {
+            border: '1px solid blue !important',
+            outline: 'none',
+        },
+
+    }
+}
 
 const Reports = () => {
+    const dataRangeOptions = ['Today', 'Yesterday', 'Last 7 days', 'Last month', 'Custom data range'];
     const [activeContent, setActiveContent] = useState('ticket reports');
+    const [dataContent, setDataContent] = useState('Yesterday');
     const handleNavigationClick = (e, content) => {
         e.preventDefault();
         setActiveContent(content);
@@ -24,7 +42,7 @@ const Reports = () => {
     const renderContent = () => {
         switch (activeContent) {
             case 'ticket reports':
-                return <TicketReports/>
+                return <TicketReports />
             case 'new tickets':
                 return <NewTickets />
             case 'tickets satisfsction':
@@ -46,9 +64,14 @@ const Reports = () => {
         }
     }
     const [isAddFilter, setIsAddFilter] = useState(false);
+    const [isActive, setIsActive] = useState(false); //toggle
     const handleAddFilter = () => {
         setIsAddFilter(!isAddFilter)
     }
+    //toggle
+    const handleToggle = () => {
+        setIsActive(!isActive);
+    };
     return (
         <div className='maincontent'>
 
@@ -57,7 +80,7 @@ const Reports = () => {
 
                 <div className='msgContL analytics__left__content'>
                     <li className='solo'>
-                    <a href='#' onClick={(e) => handleNavigationClick(e, 'ticket reports')}><span className='leftbar__item__title' >Last 7 Days</span></a></li>
+                        <a href='#' onClick={(e) => handleNavigationClick(e, 'ticket reports')}><span className='leftbar__item__title' >Last 7 Days</span></a></li>
                     <li><a href='#' onClick={(e) => handleNavigationClick(e, 'new tickets')}><span className='leftbar__item__title' >New Tickets</span></a></li>
                     <li ><a href='#' onClick={(e) => handleNavigationClick(e, 'tickets satisfsction')}><span className='leftbar__item__title'>Ticket satisfaction</span></a></li>
                     <li ><a href='#' onClick={(e) => handleNavigationClick(e, 'first response time')}><span className='leftbar__item__title'>First response time</span></a></li>
@@ -68,26 +91,49 @@ const Reports = () => {
                     <li ><a href='#' onClick={(e) => handleNavigationClick(e, 'audit log')}><span className='leftbar__item__title'>Audit log</span></a></li>
                 </div>
                 <div className='msgContR'>
-                    <div className="reports_style">
-                        <div className="header">
-                            <div className="header_left">
-                                <label>Filters:</label>
-                                <button className="add_filter_btn" onClick={handleAddFilter}>+ Add filter</button>
-                                {
-                                    isAddFilter && (
-                                        <div className="popup_container">
-                                            <ul>
-                                                <li><StraightIcon />Priority</li>
-                                                <li><LocalOfferOutlinedIcon />Tag</li>
-                                                <li><WorkspacesOutlinedIcon />Team</li>
-                                                <li><TranslateOutlinedIcon />Language</li>
-                                            </ul>
-                                        </div>
-                                    )
-                                }
+                    {
+                        activeContent !== 'ticket reports' && (
+
+                            <div className="reports_style">
+                                <div className="header">
+                                    <div className="header_left">
+                                        <label>Filters:</label>
+                                        <button className="add_filter_btn" onClick={handleAddFilter}>+ Add filter</button>
+                                        {
+                                            isAddFilter && (
+                                                <div className="popup_container">
+                                                    <ul>
+                                                        <li><StraightIcon />Priority</li>
+                                                        <li><LocalOfferOutlinedIcon />Tag</li>
+                                                        <li><WorkspacesOutlinedIcon />Team</li>
+                                                        <li><TranslateOutlinedIcon />Language</li>
+                                                    </ul>
+                                                </div>
+                                            )
+                                        }
+                                        <AutocompleteComponent
+                                            options={dataRangeOptions}
+                                            value={dataContent}
+                                            onChange={(event, newValue) => setDataContent(newValue)}
+                                            customStyles={styles.newticketsAutocomplete}
+                                        />
+                                    </div>
+                                    <div className='holidaytoggle' style={{ width: '210px' }}>
+                                        <label className="toggle-label">24-hour distribution:</label>
+                                        <button
+                                            type="button"
+                                            className={`toggle__control ${isActive ? 'active' : ''}`}
+                                            onClick={handleToggle}
+                                            aria-label="Toggle"
+
+                                        >
+                                            <div className='toggle-indicator'></div>
+                                        </button>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                    </div>
+                        )
+                    }
                     {renderContent()}
                 </div>
             </div>
