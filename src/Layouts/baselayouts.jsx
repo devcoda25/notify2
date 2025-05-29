312cf8c0ab8a130584961daaedf27c1e4e2b9f96
IamAbishake <abishake@heptotechnologies.org>
2025-05-29 13:42:31 +0530

    Multi User login Update

--- src/Layouts/baselayouts.jsx
+++ src/Layouts/baselayouts.jsx
@@ -1,115 +1,141 @@
-import React, { useEffect, useState } from 'react'
-import { BrowserRouter, Navigate, Route, Routes, useLocation } from 'react-router-dom';
-import TeamInbox from '../Pages/TeamInbox';
-import Meetings from '../Pages/Meetings';
+import React, { useEffect, useState } from "react";
+import {
+  BrowserRouter,
+  Navigate,
+  Route,
+  Routes,
+  useLocation,
+} from "react-router-dom";
+import TeamInbox from "../Pages/TeamInbox";
+import Meetings from "../Pages/Meetings";
 // import BroadcastBody from '../Component/Broadcast/BroadcastBody';
-import Broadcast from '../Pages/Broadcast';
-import Navbar from '../Component/Navbar/Navbar';
-import ContactUs from '../Pages/contactus';
-import Automations from '../Pages/automations';
-import Analytics from '../Pages/Analytics';
-import UserManagement from '../Pages/UserManagement';
-import AccountDetails from '../Pages/AccountDetails';
-import Reports from '../Pages/Reports';
-import CopyMeetingLink from '../Component/Meetings/CopyMeetingLink';
-import EditEventType from '../Component/Meetings/EditEventType';
-import Event from '../Component/Meetings/Event';
-import Meeting from '../Component/Meetings/Meeting';
-import Availability from '../Component/Meetings/Availability';
-import Analytic from '../Component/Meetings/Analytic';
-import OneononeMeeting from '../Component/Meetings/OneononeMeeting';
-import EditEvent from '../Component/Meetings/EditEvent';
-import Login from '../Pages/Login';
-import { setISODay } from 'date-fns';
+import Broadcast from "../Pages/Broadcast";
+import Navbar from "../Component/Navbar/Navbar";
+import ContactUs from "../Pages/contactus";
+import Automations from "../Pages/automations";
+import Analytics from "../Pages/Analytics";
+import UserManagement from "../Pages/UserManagement";
+import AccountDetails from "../Pages/AccountDetails";
+import Reports from "../Pages/Reports";
+import CopyMeetingLink from "../Component/Meetings/CopyMeetingLink";
+import EditEventType from "../Component/Meetings/EditEventType";
+import Event from "../Component/Meetings/Event";
+import Meeting from "../Component/Meetings/Meeting";
+import Availability from "../Component/Meetings/Availability";
+import Analytic from "../Component/Meetings/Analytic";
+import OneononeMeeting from "../Component/Meetings/OneononeMeeting";
+import EditEvent from "../Component/Meetings/EditEvent";
+import Login from "../Pages/Login";
+import { setISODay } from "date-fns";
 
 const AppRoutes = () => {
-    const location = useLocation();
-    const hideNavbarPaths = ['/hepto'];
-    const shouldHideNavbar = hideNavbarPaths.includes(location.pathname);
+  const location = useLocation();
+  const [authUser, setauthUser] = useState(null);
 
-    return (
-        <>
-            {!shouldHideNavbar && <Navbar />}
-            <Routes>
-                <Route path='/' element={<TeamInbox />} />
-                {/* <Route path='login' element={<Login />} /> */}
-                <Route path='/Teaminbox' element={<TeamInbox />} />
-                <Route path='meetings' element={<Meetings />} />
-                <Route path='Broadcast' element={<Broadcast />} />
-                <Route path='contactus' element={<ContactUs />} />
-                <Route path='automations' element={<Automations />} />
-                <Route path='analytics' element={<Analytics />} />
-                <Route path='Reports' element={<Reports />} />
-                <Route path='UserManagement' element={<UserManagement />} />
-                <Route path='AccountDetails' element={<AccountDetails />} />
-                <Route path='/hepto' element={<CopyMeetingLink />} />
-                <Route path='/edit-event' element={<EditEventType />} />
-                <Route path='/edit-eventtype' element={<EditEvent />} />
-                <Route path='/one-on-one' element={<OneononeMeeting />} />
-                <Route path='/event-types' element={<Event />} />
-                <Route path='/meet' element={<Meeting />} />
-                <Route path='/availability' element={<Availability />} />
-                <Route path='/analytic' element={<Analytic />} />
-            </Routes>
-        </>
-    );
+  
+    // const checkAuthStatus = () => {
+    //   const params = new URLSearchParams(window.location.search);
+    //   const x_authuser = params.get("x_authuser");
+    //   setauthUser(x_authuser);
+  
+    //   if (x_authuser) {
+    //     console.log("x_authuser", x_authuser);
+    //     window.location.href = `/u/${x_authuser}${
+    //       window.location.pathname !== "/" ? window.location.pathname : ""
+    //     }`;
+    //   }
+    // };
+  
+    // useEffect(() => {
+    //   checkAuthStatus();
+    // }, []);
+
+  const hideNavbarPaths = ["/hepto"];
+  const shouldHideNavbar = hideNavbarPaths.includes(location.pathname);
+
+  return (
+    <>
+      {!shouldHideNavbar && <Navbar />}
+      <Routes>
+        <Route path="/" element={<TeamInbox />} />
+        {/* <Route path='login' element={<Login />} /> */}
+        {/* <Route path={`u/${authUser}/Teaminbox`} element={<TeamInbox />} /> */}
+        <Route path={`/Teaminbox`} element={<TeamInbox />} />
+        <Route path="/extended" element={<TeamInbox />} />
+        <Route path="/chatbot" element={<TeamInbox />} />
+        <Route path="meetings" element={<Meetings />} />
+        <Route path="Broadcast" element={<Broadcast />} />
+        <Route path="contactus" element={<ContactUs />} />
+        <Route path="automations" element={<Automations />} />
+        <Route path="analytics" element={<Analytics />} />
+        <Route path="Reports" element={<Reports />} />
+        <Route path="UserManagement" element={<UserManagement />} />
+        <Route path="AccountDetails" element={<AccountDetails />} />
+        <Route path="/hepto" element={<CopyMeetingLink />} />
+        <Route path="/edit-event" element={<EditEventType />} />
+        <Route path="/edit-eventtype" element={<EditEvent />} />
+        <Route path="/one-on-one" element={<OneononeMeeting />} />
+        <Route path="/event-types" element={<Event />} />
+        <Route path="/meet" element={<Meeting />} />
+        <Route path="/availability" element={<Availability />} />
+        <Route path="/analytic" element={<Analytic />} />
+      </Routes>
+    </>
+  );
 };
 
 const BaseLayouts = () => {
-    const [isAuthenticated, setIsAuthenticated] = useState(
-        () => sessionStorage.getItem('auth') === 'true');
+  const [isAuthenticated, setIsAuthenticated] = useState(
+    () => sessionStorage.getItem("auth") === "true"
+  );
 
-    const handleLogin = () => {
-        setIsAuthenticated(true);
-    }
+  const handleLogin = () => {
+    setIsAuthenticated(true);
+  };
 
-    return (
-        // <>
-        //     <React.Fragment>
+  return (
+    // <>
+    //     <React.Fragment>
 
-        //         <BrowserRouter>
-        //         <Navbar/>
-        //             <Routes>
-        //                 <Route path='/' element={<TeamInbox />} />
-        //                 <Route path='Teaminbox' element={<TeamInbox />} />
-        //                 <Route path='meetings' element={<Meetings />} />
-        //                 <Route path='Broadcast' element={<Broadcast />} />
-        //                 <Route path='contactus' element={<ContactUs/>}/>
-        //                 <Route path='automations' element={<Automations/>}/>
-        //                 <Route path='analytics' element={<Analytics/>} />
-        //                 <Route path='Reports' element={<Reports/>}/>
-        //                 <Route path='UserManagement' element={<UserManagement/>}/>
-        //                 <Route path='AccountDetails' element={<AccountDetails/>}/>
-        //                 <Route path='/hepto' element={<CopyMeetingLink/>}/>
-        //                 <Route path='/edit-event' element={<EditEventType/>}/>
-        //                 <Route path='/edit-eventtype' element={<EditEvent/>}/>
-        //                 <Route path='/one-on-one' element={<OneononeMeeting/>}/>
-        //                 <Route path='/event-types' element={<Event/>}/>
-        //                 <Route path='/meet' element={<Meeting/>}/>
-        //                 <Route path='/availability' element={<Availability/>}/>
-        //                 <Route path='/analytic' element={<Analytic/>}/>
+    //         <BrowserRouter>
+    //         <Navbar/>
+    //             <Routes>
+    //                 <Route path='/' element={<TeamInbox />} />
+    //                 <Route path='Teaminbox' element={<TeamInbox />} />
+    //                 <Route path='meetings' element={<Meetings />} />
+    //                 <Route path='Broadcast' element={<Broadcast />} />
+    //                 <Route path='contactus' element={<ContactUs/>}/>
+    //                 <Route path='automations' element={<Automations/>}/>
+    //                 <Route path='analytics' element={<Analytics/>} />
+    //                 <Route path='Reports' element={<Reports/>}/>
+    //                 <Route path='UserManagement' element={<UserManagement/>}/>
+    //                 <Route path='AccountDetails' element={<AccountDetails/>}/>
+    //                 <Route path='/hepto' element={<CopyMeetingLink/>}/>
+    //                 <Route path='/edit-event' element={<EditEventType/>}/>
+    //                 <Route path='/edit-eventtype' element={<EditEvent/>}/>
+    //                 <Route path='/one-on-one' element={<OneononeMeeting/>}/>
+    //                 <Route path='/event-types' element={<Event/>}/>
+    //                 <Route path='/meet' element={<Meeting/>}/>
+    //                 <Route path='/availability' element={<Availability/>}/>
+    //                 <Route path='/analytic' element={<Analytic/>}/>
 
-        //             </Routes>
-        //         </BrowserRouter>
-        //     </React.Fragment>
-        // </>
-        <>
-            <BrowserRouter>
-                {
-                    isAuthenticated || window.location.pathname === '/hepto' ? (
-                        <AppRoutes />
-                    )
-                        : (
-                            <Login onLogin={handleLogin}/>
-                            // <>
-                            // <Route path="/" element={<Login onLogin={handleLogin} />} />
-                            // <Route path="*" element={<Navigate to="/" />} />
-                            // </>
-                        )
-                }
-
-            </BrowserRouter>
-        </>
-    );
-}
+    //             </Routes>
+    //         </BrowserRouter>
+    //     </React.Fragment>
+    // </>
+    <>
+      <BrowserRouter>
+        {isAuthenticated || window.location.pathname === "/hepto" ? (
+          <AppRoutes />
+        ) : (
+          <Login onLogin={handleLogin} />
+          // <>
+          // <Route path="/" element={<Login onLogin={handleLogin} />} />
+          // <Route path="*" element={<Navigate to="/" />} />
+          // </>
+        )}
+      </BrowserRouter>
+    </>
+  );
+};
 export default BaseLayouts;
--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -4,140 +4,189 @@ import { CloseIcon } from "../Component/Icon";
 import { Navigate } from "react-router-dom";
 
 const Login = ({ onLogin }) => {
-    const [showPasscode, setShowPasscode] = useState(false);
-    const [passcode, setPasscode] = useState('');
-    const [selectedToggle, setSelectedToggle] = useState(null);
+  const [showPasscode, setShowPasscode] = useState(false);
+  const [passcode, setPasscode] = useState("");
+  const [selectedToggle, setSelectedToggle] = useState(null);
+//   const [authUserId, setAuthUserId] = useState(null);
+//   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  const redirect_url = "http://localhost:3000/Teaminbox";
+  const handleContinue = () => {
+    setShowPasscode(true);
+  };
 
-    const handleContinue = () => {
-        setShowPasscode(true);
+  const checkAuthStatus = () => {
+    const params = new URLSearchParams(window.location.search);
+    const x_authuser = params.get("x_authuser");
+
+    if (x_authuser) {
+      console.log("x_authuser", x_authuser);
+      sessionStorage.setItem("auth", "true");
+    //   window.location.href = `/u/${x_authuser}${
+    //     window.location.pathname !== "/" ? window.location.pathname : ""
+    //   }`;
+      onLogin();
+      return;
     }
-    const handleInputChange = (index, value) => {
-        const numeric = value.replace(/\D/, ''); // Only digits
-        const updated = passcode.split('');
-        updated[index] = numeric;
-        const newPasscode = updated.join('');
-        setPasscode(newPasscode);
+    // setAuthUserId(null);
+    // setIsLoggedIn(false);
+  };
 
-        // move to next input
-        if (numeric && index < 5) {
-            const nextInput = document.getElementById(`pass-${index + 1}`);
-            nextInput?.focus();
-        }
+  useEffect(() => {
+    checkAuthStatus();
+  }, []);
 
-        // check passcode
-        if (newPasscode.length === 6) {
-            if (newPasscode === '123456') {
-                sessionStorage.setItem('auth', 'true');
-                onLogin();
-                sessionStorage.removeItem('selectedToggle');
-            } else {
-                alert('Invalid passcode');
-                setPasscode('');
-            }
-        }
-    };
-    useEffect(() => {
-        const toggle = sessionStorage.getItem('selectedToggle');
-        if (toggle) {
-            setSelectedToggle(toggle);
-        }
-    }, []);
-    return (
-        <>
-            <div className="login_page_container">
-                {
-                    <Modal open={showPasscode} onClose={() => setShowPasscode(false)} >
-                        <div className="modal_style_login">
-                            <IconButton onClick={() => setShowPasscode(false)} sx={{ position: 'absolute', top: 8, right: 8 }}>
-                                <CloseIcon />
-                            </IconButton>
+  const handleLogin = () => {
+    console.log("window.globalValue", window.globalValue);
+    if (!window.globalValue) {
+      window.location.href = `https://accounts.dev.evzone.app/?redirecturl=${redirect_url}`;
+    } else {
+      window.location.href = `https://myaccount.evzone.app/u/${window.globalValue}/profile?change=${redirect_url}`;
+    }
+  };
 
-                            <div>
-                                <img src='/assets/images/profile.png' alt='profile' />
+  const handleInputChange = (index, value) => {
+    const numeric = value.replace(/\D/, ""); // Only digits
+    const updated = passcode.split("");
+    updated[index] = numeric;
+    const newPasscode = updated.join("");
+    setPasscode(newPasscode);
 
-                            </div>
-                            <h4>Passcode Required</h4>
-                            <p>To access your Work Account, Please enter your Access Passcode</p>
-                            {Array.from({ length: 6 }).map((_, index) => (
-                                <input
-                                    key={index}
-                                    id={`pass-${index}`}
-                                    type="text"
-                                    maxLength="1"
-                                    value={passcode[index] || ''}
-                                    onChange={(e) =>
-                                        handleInputChange(index, e.target.value)
-                                    }
-                                    className="circle-input"
-                                    autoFocus={index === 0}
-                                />
-                            ))}
-                            <h5><a style={{ color: '#f77f00' }}><span style={{ color: 'black' }}>Forgot Access code?</span>Contact your Supervisor</a></h5>
-                        </div>
-                    </Modal>
-                }
-                <div className="left_content">
-                  <>
-                    <img src='/assets/images/Notify_login_logo.svg' alt='welcome' />
-                  
-                     {selectedToggle && (
-                        <>
+    // move to next input
+    if (numeric && index < 5) {
+      const nextInput = document.getElementById(`pass-${index + 1}`);
+      nextInput?.focus();
+    }
 
-                            <div>
-                                <table className="break_table">
-                                    <tbody><tr>
-                                        <th>Break Type</th>
-                                        <td>Lunch</td>
-                                    </tr>
-                                        <tr>
-                                            <th>Time Out</th>
-                                            <td>5:49 PM</td>
-                                        </tr>
-                                        <tr>
-                                            <th>Time Back</th>
-                                            <td>6:10 PM</td>
-                                        </tr>
-                                        <tr>
-                                            <th>Duration</th>
-                                            <td>00:21:02</td>
-                                        </tr>
-                                        <tr>
-                                            <th>Break Time allowed</th>
-                                            <td>Maxed Out</td>
-                                        </tr>
-                                        <tr>
-                                            <th>Remaining Break Time</th>
-                                            <td>00:00:00</td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-                            </div>
+    // check passcode
+    if (newPasscode.length === 6) {
+      if (newPasscode === "123456") {
+        setTimeout(() => {
+          sessionStorage.setItem("auth", "true");
+          onLogin();
+          sessionStorage.removeItem("selectedToggle");
+        }, 1000);
+      } else {
+        alert("Invalid passcode");
+        setPasscode("");
+      }
+    }
+  };
+  useEffect(() => {
+    const toggle = sessionStorage.getItem("selectedToggle");
+    if (toggle) {
+      setSelectedToggle(toggle);
+    }
+  }, []);
+  return (
+    <>
+      <div className="login_page_container">
+        {
+          <Modal open={showPasscode} onClose={() => setShowPasscode(false)}>
+            <div className="modal_style_login">
+              <IconButton
+                onClick={() => setShowPasscode(false)}
+                sx={{ position: "absolute", top: 8, right: 8 }}
+              >
+                <CloseIcon />
+              </IconButton>
 
-                        </>
-                    )} 
-                    
-                    </>
-                </div>
-                <div className="right_content">
-                    <h2>Welcome back, thameem!</h2>
-                    <p>You're almost there! Log in to access your work page</p>
-                    <div>
-                        <img src='/assets/images/profile.png' alt='profile' />
-
-                    </div>
-                    <div className="userinfo_container">
-                        <div className="userinfo">
-                            <div>Continue as Thameem </div>
-                            <p>thameem@heptotechnologies.org</p>
-                        </div>
-                        <button className="continue_button" onClick={handleContinue}>Continue</button>
-                    </div>
-                    <h4><a style={{ color: '#f77f00' }}><span style={{ color: 'black' }}>Not you?</span> Log in with a different personal account</a></h4>
-                    {/* <button className="login_btn">Login</button> */}
-                </div>
+              <div>
+                <img src="/assets/images/profile.png" alt="profile" />
+              </div>
+              <h4>Passcode Required</h4>
+              <p>
+                To access your Work Account, Please enter your Access Passcode
+              </p>
+              {Array.from({ length: 6 }).map((_, index) => (
+                <input
+                  key={index}
+                  id={`pass-${index}`}
+                  type="text"
+                  maxLength="1"
+                  value={passcode[index] || ""}
+                  onChange={(e) => handleInputChange(index, e.target.value)}
+                  className="circle-input"
+                  autoFocus={index === 0}
+                />
+              ))}
+              <h5>
+                <a style={{ color: "#f77f00" }}>
+                  <span style={{ color: "black" }}>Forgot Access code?</span>
+                  Contact your Supervisor
+                </a>
+              </h5>
             </div>
-        </>
-    )
-}
+          </Modal>
+        }
+        <div className="left_content">
+          <>
+            <img src="/assets/images/Notify_login_logo.svg" alt="welcome" />
+
+            {selectedToggle && (
+              <>
+                <div>
+                  <table className="break_table">
+                    <tbody>
+                      <tr>
+                        <th>Break Type</th>
+                        <td>Lunch</td>
+                      </tr>
+                      <tr>
+                        <th>Time Out</th>
+                        <td>5:49 PM</td>
+                      </tr>
+                      <tr>
+                        <th>Time Back</th>
+                        <td>6:10 PM</td>
+                      </tr>
+                      <tr>
+                        <th>Duration</th>
+                        <td>00:21:02</td>
+                      </tr>
+                      <tr>
+                        <th>Break Time allowed</th>
+                        <td>Maxed Out</td>
+                      </tr>
+                      <tr>
+                        <th>Remaining Break Time</th>
+                        <td>00:00:00</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </>
+            )}
+          </>
+        </div>
+        <div className="right_content">
+          <h2>Welcome back, thameem!</h2>
+          <p>You're almost there! Log in to access your work page</p>
+          <div>
+            <img src="/assets/images/profile.png" alt="profile" />
+          </div>
+          <div className="userinfo_container">
+            <div className="userinfo">
+              <div>Continue as Thameem </div>
+              <p>thameem@heptotechnologies.org</p>
+            </div>
+            <button className="continue_button" onClick={handleContinue}>
+              Continue
+            </button>
+          </div>
+          x
+          <h4>
+            <a style={{ color: "#f77f00" }}>
+              <span style={{ color: "black" }}>Not you?</span> Log in with a
+              different personal account
+            </a>
+          </h4>
+          <button onClick={handleLogin} className="login_btn">
+            Login
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
 export default Login;
